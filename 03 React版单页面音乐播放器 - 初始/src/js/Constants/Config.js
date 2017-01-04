8966b9965e6f1276a7389bd4d098bfb021175799
Celbyo <809908337@qq.com>
2017-01-05 00:29:46 +0530

    remove new gitignore directory

--- 03 React版单页面音乐播放器 - 初始/src/js/Constants/Config.js
+++
@@ -1,6 +0,0 @@
-// 配置
-export const CONFIG = {
-	base_url: 'http://tingapi.ting.baidu.com/v1/restserver/ting',
-	song_method: 'baidu.ting.song.play',
-	query_method: 'baidu.ting.search.catalogSug',
-};
